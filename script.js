e8663bb14f1fa9c50c5c7a5e8dadd12e41d31495
JoseMSM <nueljosema@gmail.com>
2024-04-15 13:51:43 -0600

    Added the title of the project, button for asking how many squares would the user like (not functioning yet)

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const container = document.querySelector('.container-screen');
+const squaresBtn = document.querySelector('squaresBtn');
 
 for (let i = 0; i < 16; i++) {
     for(let j = 0; j < 16; j++) {
         const cell = document.createElement('div');
         cell.classList.add('cell');
         container.appendChild(cell);
+        cell.addEventListener('mouseover',
+    e => e.target.classList.add('my-color-class'));
     }
 }
